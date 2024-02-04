5d95d34771b2820dcaf35c06f5d799726f578aaa
Kishor0100 <kishorchary2001@gmail.com>
2024-02-04 17:36:02 +0000

    solution

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO restaurant(name,address,cuisineType,rating)
+VALUES('Fine dining','123 main 5t','European',5);
+INSERT INTO restaurant(name,address,cuisineType,rating)
+VALUES('Taco bell','456 elm 5t','Fast food',3);
+INSERT INTO restaurant(name,address,cuisineType,rating)
+VALUES('Suhsi place','789 ook 5t','Japanese',4);
+INSERT INTO chef(firstName,lastName,expertise,experienceYears,restaurantId)
+VALUES('John','Doe','Sous chef',5,1);
+INSERT INTO chef(firstName,lastName,expertise,experienceYears,restaurantId)
+VALUES('Jane','Doe','Pastry chef',7,1);
+INSERT INTO chef(firstName,lastName,expertise,experienceYears,restaurantId)
+VALUES('Mike','Smith','Head chef',10,2);
+INSERT INTO chef(firstName,lastName,expertise,experienceYears,restaurantId)
+VALUES('Emily','Johnson','Sushi chef',8,3);
+INSERT INTO chef(firstName,lastName,expertise,experienceYears,restaurantId)
+VALUES('Anna','Williams','Pastry chef',6,3);
+INSERT INTO chef(firstName,lastName,expertise,experienceYears,restaurantId)
+VALUES('Mark','Brown','Sous chef',4,2);
